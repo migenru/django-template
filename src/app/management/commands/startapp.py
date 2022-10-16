b277fee7440d34a19a6be04516ad11bf03866108
migenru <50385301+migenru@users.noreply.github.com>
2022-10-16 19:11:35 +0300

    Update startapp.py

--- src/app/management/commands/startapp.py
+++ src/app/management/commands/startapp.py
@@ -8,6 +8,6 @@ class Command(BaseCommand):
     """Set custom template for all newly generated apps"""
     def handle(self, **options):
         if 'template' not in options or options['template'] is None:
-            options['template'] = path.join(settings.BASE_DIR, '.django-app-template')
+            options['template'] = path.join(settings.ROOT_DIR, '.django-app-template')
 
         super().handle(**options)
